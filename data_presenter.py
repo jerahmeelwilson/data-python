bcc72722745f802e1c5d1a7d1de60e35dbea5abf
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-11-11 15:12:00 -0600

    Added bar graph

--- data_presenter.py
+++ data_presenter.py
@@ -26,6 +26,33 @@ with open("CupcakeInvoices.csv") as invoices:
 
 print(total)
 
+chocolateTotal = 0
+vanillaTotal = 0
+strawberryTotal = 0
+
+with open("CupcakeInvoices.csv") as invoices:
+    reader = csv.reader(invoices)
+    for row in reader:
+        if(row[2] == "Chocolate"):
+            chocolateTotal = round(chocolateTotal + int(row[3]) * float(row[4]),2)
+        elif(row[2] == "Vanilla"):
+            vanillaTotal = round(vanillaTotal + int(row[3]) * float(row[4]),2)
+        else:
+            strawberryTotal = round(strawberryTotal + int(row[3]) * float(row[4]),2)
+
+
+print(chocolateTotal, vanillaTotal, strawberryTotal)
+
+import plotly.graph_objects as go
+fig = go.Figure(
+   data=[go.Bar(y=[chocolateTotal, vanillaTotal, strawberryTotal])],
+   layout_title_text="Chocolate vs Vanilla vs Strawberry Cupcake Sales"
+)
+fig.show()
+
+
+
+
 
 
 invoices.close()
